34cda723218ce700541ed22645bfc9fc2ffc04f2
Zsolt Hanasz <hanaszzsolt323@gmail.com>
2025-07-11 19:30:33 +0200

    added routes folder

---
+++ routes/ideas.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+
+const ideas = [
+  {
+    id: 1,
+    text: 'I dont really understand any of this but whatever',
+    tag: 'Technology',
+    username: 'YourMom12',
+    date: '2032-13-12'
+  },
+  {
+    id: 2,
+    text: 'Flying without wings or anything should be possible',
+    tag: 'Invention',
+    username: 'FlyFar1223',
+    date: '2123-23-23'
+  },
+  {
+    id: 3,
+    text: 'Great idea, we should try it right now',
+    tag: 'Infamous Last Words',
+    username: 'JustDoIt23',
+    date: '2028-12-12'
+  }
+]
+
+// Get all ideas
+router.get('/', (req, res) => {
+  res.json({ success: true, data: ideas })
+})
+// Get single idea
+router.get('/:id', (req, res) => {
+  const idea = ideas.find(idea => idea.id === +req.params.id)
+
+  if(!idea) {
+    return res.status(404).json({success: false, error: 'Resource not found'})
+  }
+
+  res.json({ success: true, data: idea })
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,46 +3,11 @@ const port = 5000;
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: 'I dont really understand any of this but whatever',
-    tag: 'Technology',
-    username: 'YourMom12',
-    date: '2032-13-12'
-  },
-  {
-    id: 2,
-    text: 'Flying without wings or anything should be possible',
-    tag: 'Invention',
-    username: 'FlyFar1223',
-    date: '2123-23-23'
-  },
-  {
-    id: 3,
-    text: 'Great idea, we should try it right now',
-    tag: 'Infamous Last Words',
-    username: 'JustDoIt23',
-    date: '2028-12-12'
-  }
-]
+
 
 app.get('/', (req, res) => { 
   res.json({ message: 'Welcome to the RandomIdeasAPI' })
 });
-// Get all ideas
-app.get('/api/ideas', (req, res) => {
-  res.json({ success: true, data: ideas })
-})
 
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find(idea => idea.id === +req.params.id)
-
-  if(!idea) {
-    return res.status(404).json({success: false, error: 'Resource not found'})
-  }
-
-  res.json({ success: true, data: idea })
-})
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
