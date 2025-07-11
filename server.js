56cdf7131d7b1a22364b50fadc2c7533d2062b7b
Zsolt Hanasz <hanaszzsolt323@gmail.com>
2025-07-11 20:41:56 +0200

    added delete and put request

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ const ideasRouter = require('./routes/ideas')
 app.use('/api/ideas', ideasRouter)
 
 
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
+app.listen(port, () => console.log());
