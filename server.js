5df34c2b2123ac26f243f8f0d8b8a33b605a3d7a
Zsolt Hanasz <hanaszzsolt323@gmail.com>
2025-07-11 19:36:35 +0200

    fixed

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeasAPI' })
 });
 
+const ideasRouter = require('./routes/ideas')
+app.use('/api/ideas', ideasRouter)
+
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
